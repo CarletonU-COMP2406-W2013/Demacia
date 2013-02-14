1add140ddd420fb8747841ebc94adbeac6249d4e
Francis Lavoie <lavofr@gmail.com>
2013-02-14 06:47:37 -0500

    Moved out hardcoded HTML from the javascript to Jade.

--- DemaciaTV/public/javascripts/twitch-main.js
+++ DemaciaTV/public/javascripts/twitch-main.js
@@ -1,3 +1,68 @@
+$(document).ready(function() {
+
+  Twitch.init({clientId: 'j0yll37nxeynttp6x4jb9sj4d4v9ev3'}, function(error, status) {
+    if (error) {
+      // error encountered while loading
+      console.log(error);
+    }
+    // the sdk is now loaded
+    if (status.authenticated) {
+      // already logged in, hide button
+      //$('.twitch-connect').hide()
+      Twitch.api({method: 'user'}, function(error, user) {
+        $('.connected').text(' Connected as ' + user.display_name);
+      });
+    }
+  });
+  
+  // Add the default stream and chat
+  DemaciaTV.addStream('riotgames', '1', 'Riot Games');
+  DemaciaTV.addChat('riotgames', '1');
+  DemaciaTV.addStream('dreamhacktv', '2', 'DreamHack');
+  //DemaciaTV.addChat('dreamhacktv', '2');
+
+  // Make the connect button work
+  $('.twitch-connect').click(function() {
+    Twitch.login({
+      //popup: true,
+      scope: ['user_read', 'channel_read']
+    });
+  });
+  
+  // Sound toggle
+  var container = $('#stream-container_1').data('mute', 'false')
+    , player = $('#stream_1')[0];
+  window.setTimeout(function() { player.unmute(); }, 3000);
+  $('p.stream-controls').append('<a href="javascript:void(0)" id="soundtoggle">Toggle audio</a>');
+  $('#soundtoggle').click(function() {
+    if(container.data('mute') === 'true') {
+      player.unmute();
+      container.data('mute', 'false');
+    } else {
+      player.mute();
+      container.data('mute', 'true');
+    }
+  });
+  
+  // Hider
+  $('p.stream-controls').append('<br /><a href="javascript:void(0)" id="streamtoggle"> Toggle stream</a>');
+  $('#streamtoggle').click(function() {
+    $('#stream-container_1').toggle();
+  });
+
+  // Slide to close the chat and expand other sections
+  $('#chat-toggle-left').hide();
+  $('#chat-toggle-right').click(DemaciaTV.toggleChat);
+  $('#chat-toggle-left').click(DemaciaTV.toggleChat);
+  
+  // Change stream on enter in the text box
+  $('#picker').keydown(function (e){
+    if(e.keyCode === 13) {
+      DemaciaTV.changeStream($(this).val(), '1');
+    }
+  });
+});
+
 var DemaciaTV = (function () {
   return {
     changeStream: function(channel, cindex) {
@@ -56,70 +121,3 @@ var DemaciaTV = (function () {
     }
   };
 }());
-
-$(document).ready(function() {
-
-  Twitch.init({clientId: 'j0yll37nxeynttp6x4jb9sj4d4v9ev3'}, function(error, status) {
-    if (error) {
-      // error encountered while loading
-      console.log(error);
-    }
-    // the sdk is now loaded
-    if (status.authenticated) {
-      // already logged in, hide button
-      //$('.twitch-connect').hide()
-      Twitch.api({method: 'user'}, function(error, user) {
-        $('.connected').text('Connected as ' + user.display_name);
-      });
-    }
-  });
-  
-  // Add the default stream and chat
-  DemaciaTV.addStream('riotgames', '1', 'Riot Games');
-  DemaciaTV.addChat('riotgames', '1');
-  DemaciaTV.addStream('dreamhacktv', '2', 'DreamHack');
-  //DemaciaTV.addChat('dreamhacktv', '2');
-
-  // Make the connect button work
-  $('.twitch-connect').click(function() {
-    Twitch.login({
-      //popup: true,
-      scope: ['user_read', 'channel_read']
-    });
-  });
-  
-  // Sound toggle
-  var container = $('#stream-container_1').data('mute', 'false')
-    , player = $('#stream_1')[0];
-  window.setTimeout(function() { player.unmute(); }, 3000);
-  $('p.stream-controls').append('<a href="javascript:void(0)" id="soundtoggle">Toggle audio</a>');
-  $('#soundtoggle').click(function() {
-    if(container.data('mute') === 'true') {
-      player.unmute();
-      container.data('mute', 'false');
-    } else {
-      player.mute();
-      container.data('mute', 'true');
-    }
-  });
-  
-  // Hider
-  $('p.stream-controls').append('<br /><a href="javascript:void(0)" id="streamtoggle"> Toggle stream</a>');
-  $('#streamtoggle').click(function() {
-    $('#stream-container_1').toggle();
-  });
-
-  // Slide to close the chat and expand other sections
-  $('#header').append('<a href="javascript:void(0)" id="chat-toggle-left"></a>');
-  $('#header').append('<a href="javascript:void(0)" id="chat-toggle-right"></a>');
-  $('#chat-toggle-left').hide();
-  $('#chat-toggle-right').click(DemaciaTV.toggleChat);
-  $('#chat-toggle-left').click(DemaciaTV.toggleChat);
-  
-  // Change stream on enter in the text box
-  $('#picker').keydown(function (e){
-    if(e.keyCode === 13) {
-      DemaciaTV.changeStream($(this).val(), '1');
-    }
-  });
-});
